de77dd7509d37cbfb741ebf57d0a4e5de4e19320
Jeff Lang <jeffplang@gmail.com>
2016-03-01 15:45:32 -0500

    basic skeleton of lambda packaging code

---
+++ assets/lambda-package/index.js
@@ -0,0 +1,19 @@
+var path = require('path');
+var FastBootServer = require('ember-fastboot-server');
+var outputPath = 'fastboot-dist';
+var appName = 'bustle';
+
+var server = new FastBootServer({
+  distPath: 'fastboot-dist'
+});
+
+function insertIntoIndexHTML(res) {
+  return server.insertIntoIndexHTML(res.title, res.body, res.head);
+}
+
+exports.handler = function(event, context) {
+  server.app.visit(event.path, { request: { get: function() {} }, response: {} })
+    .then(insertIntoIndexHTML)
+    .then(context.succeed)
+    .catch(context.fail);
+};
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 /* jshint node: true */
 'use strict';
 
+var FastbootLambdaDeployPlugin = require('./lib/fastboot-lambda-deploy-plugin');
+
 module.exports = {
-  name: 'ember-cli-deploy-fastboot-lambda'
+  name: 'ember-cli-deploy-fastboot-lambda',
+
+  createDeployPlugin: function(options) {
+    return new FastbootLambdaDeployPlugin({
+      name: options.name
+    });
+  }
 };
